2c8926f2a14268cdc3e2cb0d7d73bf6aeab19914
WH1024 <1258222155@qq.com>
2021-10-19 05:42:24 -0700

    first commit

---
+++ test.c
@@ -0,0 +1,13 @@
+/* ************************************************************************
+> File Name:     test.c
+> Author:        huangweihan
+> 
+> Created Time:  Tue 19 Oct 2021 04:41:06 AM PDT
+> Description:   
+ ************************************************************************/
+#include<stdio.h>
+
+int main(int argc, char const* argv[]){
+    printf("hello world\n");
+    return 0;
+}
