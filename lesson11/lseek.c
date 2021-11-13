5dbfc8b38f1dab51918af1e68ab8f4c15f6e0443
WH1024 <1258222155@qq.com>
2021-11-13 08:06:33 -0800

    1113

--- lesson11/lseek.c
+++ lesson11/lseek.c
@@ -26,8 +26,44 @@
                     设置偏移量：文件大小+第二个参数offset的值
     作用：
         1. 移动文件指针到头文件
+        lseek(fd, 0, SEEK_SET);
 
-                    
- 
+        2. 获取当前文件指针的位置
+        lseek(fd, 0, SEEK_CUR);
+         返回值： 返回文件指针的位置
+
+        3. 获取文件长度
+        lseek(fd, 0, SEEK_END);
+        
+        4. 拓展文件的长度，当前文件10b，110b, 增加了100个字节
+        lseek(fd, 100, SEEK_END);
+
+
  */
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdio.h>
 
+int main(){
+    
+    int fd = open("hello.txt", O_RDWR);
+    if(fd == -1){
+        perror("open");
+        return -1;
+    }
+    // 扩展文件的长度
+    int ret = lseek(fd, 100, SEEK_END);
+    if(ret == -1){
+        perror("open");
+        return -1;
+    }
+
+    //写入一个空数据
+    write(fd," ", 1);
+    //关闭文件
+    close(fd);
+
+    return 0;
+}
